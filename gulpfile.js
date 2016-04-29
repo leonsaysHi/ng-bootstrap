960389cb9db14e821471532127690a42fda877e4
lronssin <leon.ronssin@b-i.com>
2016-04-29 16:05:44 +0200

    switch to browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp')
-var connect = require('gulp-connect')
+var browserSync = require('browser-sync')
 var useref = require('gulp-useref')
 var sass = require('gulp-sass')
 var angularTemplateCache = require('gulp-angular-templatecache')
@@ -9,31 +9,32 @@ var angularTemplateCache = require('gulp-angular-templatecache')
 gulp.task('copy', function() {
     return gulp.src('./app/*.html')
         .pipe(useref())
-        .pipe(gulp.dest('./public'));
+        .pipe(gulp.dest('./public'))
 })
 
 // Dev
-gulp.task('watch', function() {
-    gulp.watch('./app/scripts/**/*.tpl.html', ['tpls'])
-    gulp.watch('./app/sass/**/*.scss', ['sass'])
-})
+
 gulp.task('tpls', function() {
   return gulp.src('./app/scripts/**/*.tpl.html')
     .pipe(angularTemplateCache({module:'tpls', standalone:true}))
-    .pipe(gulp.dest('./app/scripts'));
+    .pipe(gulp.dest('./app/scripts'))
 })
 gulp.task('sass', function() {
     return gulp.src('./app/sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./app/css'))
 })
-gulp.task('connect', function () {
-    connect.server({
-        root: './app',
-        port: 4000
+gulp.task('serve', function() {
+    browserSync.init({
+      server: "./app/"
     })
+    gulp.watch('./app/scripts/**/*.tpl.html', ['tpls']).on('change', browserSync.reload)
+    gulp.watch('./app/sass/**/*.scss', ['sass']).on('change', browserSync.reload)
+
 })
 
-gulp.task('build', ['copy']);
-gulp.task('dev', ['sass', 'tpls']);
-gulp.task('default', ['dev', 'connect', 'watch']);
+// Tasks
+
+gulp.task('build', ['copy'])
+gulp.task('dev', ['sass', 'tpls'])
+gulp.task('default', ['dev', 'serve'])
